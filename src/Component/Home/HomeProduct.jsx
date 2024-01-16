6bac5df99336e65e75b7f74ad45a6a54abb28393
Sanket Pathare <126545815+sanket8808@users.noreply.github.com>
2024-01-16 22:39:55 +0530

    new

--- src/Component/Home/HomeProduct.jsx
+++ src/Component/Home/HomeProduct.jsx
@@ -9,17 +9,17 @@ const HomeProduct = [
   },
   {
     id: 2,
-    Title: "Watch SE (GPS, 44mm)",
-    cat: "Watch",
-    price: "₹32,000",
+    Title: "iPhone 15 Pro (256 GB) - Natural Titanium",
+    cat: "Phone",
+    price: "₹1,40,990",
     Img: "https://m.media-amazon.com/images/I/81CgtwSII3L._AC_UY327_FMwebp_QL65_.jpg"
   },
   {
     id: 3,
-    Title: "s",
-    cat: "Tablet",
-    price: "₹5990",
-    Img: "../../img/laptop.png"
+    Title: "Rog Strix G15, AMD Ryzen 7 6800H",
+    cat: "Laptop",
+    price: "₹83,990 ",
+    Img: "https://m.media-amazon.com/images/I/71m7XVxRD-L._AC_UY327_FMwebp_QL65_.jpg"
   },
   {
     id: 4,
